d4ec52638c4946ed712ca77789772ab41c945642
jvercide <jerome8vercide@yahoo.com>
2017-12-08 14:00:43 -0500

    sample html with live reload

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,8 @@ const hello = () => {
 	return(
 		<div>
 			<img src={sample} alt=""/>
-			<h1>Lets Start</h1>
+			<h1>Bootstrap starter template</h1>
+        <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './assets/stylesheets/styles.scss';
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App.jsx'
-
+import App from './app/App.jsx';
 if (process.env.NODE_ENV !== 'production') {
-  require('../build/index.html')
+	console.log("true");
+  require('../build/index.html');
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
